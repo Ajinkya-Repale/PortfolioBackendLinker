8d1053fcf5b35fe5966a3d6b983f55d792a2056b
Ajinkya-Repale <ajinkyarepale4554@gmail.com>
2026-03-18 13:31:20 +0530

    new commit atlast

--- src/test/java/com/MainApp/PortfolioApplicationTests.java
+++ src/test/java/com/MainApp/PortfolioApplicationTests.java
@@ -2,12 +2,13 @@ package com.MainApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PortfolioApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
